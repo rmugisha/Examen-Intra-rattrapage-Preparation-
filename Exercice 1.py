e5c6de890bbcccc0b5b467515260ac74b96a3c6e
git <muroxana2@gmail.com>
2021-04-13 14:04:33 -0500

    Print("Hello world")

---
+++ Exercice 1.py
@@ -0,0 +1,20 @@
+a = 2.5
+b = -1.5
+c = 2
+"Question1"
+
+"a"
+print(a + (b ** 2) - c / 3)
+
+"b"
+print(5 * a + c // b)
+
+"c"
+print(a ** b)
+
+"Question2"
+PV = 5
+INT = 2.5
+YRS = 25
+"a"
+print(PV * (1 + INT/100) ** YRS)
