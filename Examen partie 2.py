c690f8d63620516e150c916abc08f7da1443b6f2
git <muroxana2@gmail.com>
2021-04-14 15:51:38 -0500

    Print("Hello world")

---
+++ Examen partie 2.py
@@ -0,0 +1,113 @@
+a, b, c = 10, 20, 30
+print(10, 20, 30)
+
+x = "Bonjour"
+print(x[-1])
+
+x = 16 / 4 * (1 + 2) + 5
+print(x)
+
+a, b = 16, 20
+print(a + b)
+
+# def dummy(a, b):
+#    print (a * b)
+# dummy(5)
+
+def dummy(a, b):
+   c = a * b
+   print(c)
+dummy(5, 3)
+
+
+# ma_liste = [“courges”, “poireaux”, “pommes”]
+# ma_liste.append(“poires”)
+
+a = (2 * 4)
+print(a)
+
+
+# for item in ma_liste:
+#     ma_liste = [“poires”, “courges”, “poireaux”, “pommes”]
+
+
+# class Test:
+# 	def __init__(self, a):
+# 		self.a = a
+#
+# mon_test_1 = Test(12)
+# mon_test_2 = Test(5)
+# print(mon_test_1.a, mon_test_2.a)
+
+age = 2
+if age < 0:
+  message = "Merci d'entrer un age positif"
+elif age < 3:
+  message = "Tarif : gratuit"
+elif age < 18:
+  message = "Tarif : réduit - junior"
+elif age > 65:
+  message = "Tarif : réduit - senior"
+else:
+  message = "Tarif normal"
+
+# def salutations(nom, prenom):
+# 	print(‘Bonjour ’ + nom + “ ” + prenom)
+# salutations(Jean Dupont)
+
+# class Hello(object):
+#     pass
+#
+#
+# class Toto:
+# 	def show(self):
+# 		print(“Hello”)
+#
+# t = Toto()
+
+
+
+d = {"a": 1,"b": 2,"c": 4}
+print(len(d))
+
+# x = 16
+# x = “Ne soyez pas dégouté(e)s de python…”
+# print(x)
+
+
+# s1 = {"a": 1, "b": 2, "c": 4}
+# s2 = {"a": 13, "b": 2, "c": 4}
+# s1 == s2
+
+
+
+
+# s1 = {"a": 1, "b": 2, "c": 4}
+# print(s1["d"])
+
+# s1 = {"a": 1}
+# for k, v in s1.items():
+# print(“{} = {}”.format(k, v))
+
+# for i in range(1, 5):
+# print("i")
+
+
+
+# ma_liste = [“courges”, “poireaux”, “pommes”]
+# ma_liste.append(“poires”)
+
+
+# a = int(16)
+#
+# d = {"jean": [1, "x"], "jeanne" [2, "y"]}
+#
+#
+# def r(d):
+#     return d
+#
+#
+# print((d))
+
+
+
